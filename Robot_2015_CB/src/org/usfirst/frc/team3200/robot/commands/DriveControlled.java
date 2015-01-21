47a57fec6229a2f4c6a5276dfe2c65c7ec1d08c1
ablackman <ablackman@cherrycreekschools.org>
2015-01-20 18:25:22 -0700

    Initial Commit to Github
    Uploaded original project

---
+++ Robot_2015_CB/src/org/usfirst/frc/team3200/robot/commands/DriveControlled.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team3200.robot.commands;
+
+import org.usfirst.frc.team3200.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+
+/**
+ *
+ */
+public class DriveControlled extends Command {
+
+    public DriveControlled() {
+        super("DriveControlled");
+        requires(Robot.drive);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        Robot.drive.mecanumDrive(Robot.oi.getController());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
