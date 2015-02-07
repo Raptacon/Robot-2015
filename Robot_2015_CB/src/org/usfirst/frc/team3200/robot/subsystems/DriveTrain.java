e1d9f48ba79404d16cc426002c33f8ff438fb628
clkinder1 <clkinder1@gmail.com>
2015-02-07 10:38:12 -0700

    Talon Controller

--- Robot_2015_CB/src/org/usfirst/frc/team3200/robot/subsystems/DriveTrain.java
+++ Robot_2015_CB/src/org/usfirst/frc/team3200/robot/subsystems/DriveTrain.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -15,7 +16,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class DriveTrain extends Subsystem {
     //declare drive train controller
-    private RobotDrive drive;
+//    private RobotDrive drive;
     
     //declare motor encoders (counts motor revolutions)
     private Encoder frontRight;
@@ -23,16 +24,26 @@ public class DriveTrain extends Subsystem {
     private Encoder frontLeft;
     private Encoder rearLeft;
     
+    private Talon fRight;
+    private Talon fLeft;
+    private Talon bRight;
+    private Talon bLeft;
+    
     //average number of meters per encoder pulse
     double distPerPulse = 0.001056942;
     
     public DriveTrain() {
     	super("DriveTrain");
     	//Initialize motors and invert left side motors
-        drive = new RobotDrive(RobotMap.FRONT_LEFT, RobotMap.BACK_LEFT, 
-                               RobotMap.FRONT_RIGHT, RobotMap.BACK_RIGHT);
-        drive.setInvertedMotor(MotorType.kFrontLeft, true);
-        drive.setInvertedMotor(MotorType.kRearLeft, true);
+//        drive = new RobotDrive(RobotMap.FRONT_LEFT, RobotMap.BACK_LEFT, 
+//                               RobotMap.FRONT_RIGHT, RobotMap.BACK_RIGHT);
+//        drive.setInvertedMotor(MotorType.kFrontLeft, true);
+//        drive.setInvertedMotor(MotorType.kRearLeft, true);
+        
+        fRight = new Talon(RobotMap.FRONT_RIGHT);
+        fLeft = new Talon(RobotMap.FRONT_LEFT);
+        bRight = new Talon(RobotMap.BACK_RIGHT);
+        bLeft = new Talon(RobotMap.BACK_LEFT);
         
         //initialize encoders and set pulse distance
         frontRight = new Encoder(RobotMap.FR_ENCODER_A, RobotMap.FR_ENCODER_B);
@@ -45,32 +56,85 @@ public class DriveTrain extends Subsystem {
         rearLeft.setDistancePerPulse(distPerPulse);
     }
     
-    //sets DriveControlled as the default command for this subsystem
-    public void initDefaultCommand() {
-        setDefaultCommand(new DriveControlled());
-    }
-    
-    //sets the direction and the rotation of the drive train
-    public void mecanumDrive(float x, float y, float rot) {
-    	drive.mecanumDrive_Cartesian(x, -y, -rot, 0);
-    }
+//    //sets DriveControlled as the default command for this subsystem
+//    public void initDefaultCommand() {
+//        setDefaultCommand(new DriveControlled());
+//    }
+//    
+//    //sets the direction and the rotation of the drive train
+//    public void mecanumDrive(float x, float y, float rot) {
+//    	drive.mecanumDrive_Cartesian(x, -y, -rot, 0);
+//    }
 
-    //sets the direction and the rotation of the drive train using values from a controller
-    public void mecanumDrive(Joystick controller) {
-        double x =  controller.getRawAxis(RobotMap.LEFT_STICK_X);
-        double y =  -controller.getRawAxis(RobotMap.LEFT_STICK_Y);
-        double rot = -controller.getRawAxis(RobotMap.RIGHT_STICK_X);
-        drive.mecanumDrive_Cartesian(x, y, rot , 0); //drive.mecanumDrive_Cartesian(x, y, rot , Robot.sensors.getGyroAngle());
-//        System.out.print(frontLeft.getRate() + "    ");
-//        System.out.println(frontRight.getRate());
-//        System.out.print(rearLeft.getRate() + "    ");
-//        System.out.println(rearRight.getRate());
-//        System.out.println();
-    }
+//    //sets the direction and the rotation of the drive train using values from a controller
+//    public void mecanumDrive(Joystick controller) {
+//        double x =  controller.getRawAxis(RobotMap.LEFT_STICK_X);
+//        double y =  -controller.getRawAxis(RobotMap.LEFT_STICK_Y);
+//        double rot = -controller.getRawAxis(RobotMap.RIGHT_STICK_X);
+//        drive.mecanumDrive_Cartesian(x, y, rot , 0); //drive.mecanumDrive_Cartesian(x, y, rot , Robot.sensors.getGyroAngle());
+//    }
     
-    //stops all motors
-    public void stop() {
-    	drive.mecanumDrive_Cartesian(0, 0, 0, 0);
+//    //stops all motors
+//    public void stop() {
+//    	drive.mecanumDrive_Cartesian(0, 0, 0, 0);
+//    }
+    
+    public void drive(Joystick controller){
+    
+    	if(controller.getRawAxis(RobotMap.LEFT_STICK_X) > 0.1 || controller.getRawAxis(RobotMap.LEFT_STICK_X) < -0.1)
+    	{
+    		if(controller.getRawAxis(RobotMap.LEFT_STICK_Y) < 0.1 && controller.getRawAxis(RobotMap.LEFT_STICK_Y) > 0.1)
+    		{	
+    			resetEncoders();
+    			//Todo Strafe code.
+    			if(controller.getRawAxis(RobotMap.LEFT_STICK_X) > 0.1)
+    			{
+    				//The robot should go right.
+    				fRight.set(controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				fLeft.set(-controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				bRight.set(-controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				bLeft.set(controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				if(frontRight.get() < -frontLeft.get())
+    				{
+    					fLeft.set(fLeft.get() * (frontLeft.get()/-frontRight.get()));
+    					
+    				}else if(frontRight.get() > -frontLeft.get())
+    				{
+    					fRight.set(fRight.get() * (frontRight.get()/-frontLeft.get()));
+    					
+    				}else if(frontRight.get() < -backRight.get())
+    				{
+    					fRight.set(fRight.get() * (frontRight.get()/-backRight.get())));
+    				}else if(frontRight.get() > -backRight.get())
+    				{
+    					bRight.set(fRight.get() - (backRight.get()/-frontRight.get()));
+    				}else if(frontLeft.get() < -backLeft.get())
+    				{
+    					fLeft.set(fLeft.get() - (backLeft.get()/-frontLeft.get()));
+    				}else if(frontLeft.get() > -backLeft.get())
+    				{
+    					bLeft.set(bLeft.get() + (frontLeft.get()/-backLeft.get()));
+    				}else if(backLeft.get() < -backRight.get())
+    				{
+    					bLeft.set(bLeft.get() + (backRight.get()/-backLeft.get()));
+    				}else if(backLeft.get() > -backRight.get())
+    				{
+    					bRight.set(bRight.get() - (backLeft.get()/-backRight.get()));
+    				}
+    				
+    				
+    			}else if(controller.getRawAxis(RobotMap.LEFT_STICK_X) < 0.1)
+    			{
+    				//The robot should go left
+    				fRight.set(-controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				fLeft.set(controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				bRight.set(controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    				bLeft.set(-controller.getRawAxis(RobotMap.LEFT_STICK_X));
+    			}
+    		}
+    	}else{
+    		
+    	}
     }
     
     //resets all encoders to zero; used to begin a new measurement
